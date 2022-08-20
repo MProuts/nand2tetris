e1f51ea2ae43d3e491a3bfef1d9cdd0700ebee9b
Michael Prouty <michael.prouty@gmail.com>
2022-08-20 16:35:16 -0400

    Finish Chapter 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,67 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// If KBD == 0, GOTO WHITE
+(START)
+  @KBD
+  D=M
+  @WHITE
+  D;JEQ
+
+  // ELSE set the substitution value to -1
+  @sub
+  M=-1
+  @REFRESH
+  0;JMP
+
+  (WHITE)
+    // set the substitution value to 0
+    @sub
+    M=0
+    @REFRESH
+    0;JMP
+
+  (REFRESH)
+  // set up loop variables
+  @i
+  M=0
+  @8159
+  D=A
+  @words
+  M=D
+
+  (LOOP)
+    // if i >= words jump to start
+    @words
+    D=M
+    @i
+    D=D-M
+    @START
+    D;JLE
+
+    // set screen address to manipulate
+    @SCREEN
+    D=A
+    @i
+    D=D+M
+    @addr
+    M=D
+
+    // set screen address to substitution value
+    @sub
+    D=M
+    @addr
+    A=M
+    M=D
+
+    // increment loop index
+    @i
+    M=M+1
+
+    @LOOP
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,57 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// i=0
+@i
+M=0
+
+// n=R1
+@R1
+D=M
+@n
+M=D
+
+// sum=0
+@sum
+M=0
+
+// while i < n
+(LOOP)
+  // D=i
+  // M=n
+  @i
+  D=M
+  @n
+  // if n - i < 0
+  // goto STOP
+  D=M-D
+  @STOP
+  D;JLE
+    // else
+
+    //  sum += R0
+    //  i+=1
+    @R0
+    D=M
+    @sum
+    M=D+M
+    @i
+    M=M+1
+  
+
+    @LOOP
+    0;JMP
+
+// end
+(STOP)
+
+// R2 = sum
+@sum
+D=M
+@R2
+M=D
+
+// EOF
+(END)
+  @END
+  0;JMP
